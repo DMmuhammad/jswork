23002c07a755689374b885fac8d7262ab2f40295
Muhammad.frontend <128728730+MagaFrontEnd@users.noreply.github.com>
2024-12-03 21:08:47 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* 
+String
+Number
+BigInt
+Boolean 
+Undefined
+Null 
+Symbol
+Object
+*/
+
+// вывести в консоль
+console.log("hello world!"); // String
+
+let admin = "Muhammad";
+let age = 19;
+
+console.log(admin, age);
+
+// let a = 30; // 40
+// let b = 10; // 30
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log("a:", a, "b:", b);
+
+// let 1dfg = 123;
+// let -reset = '234';
