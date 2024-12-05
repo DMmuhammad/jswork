07ca4366b05ec78055b3e593ead19b23ae92e97c
Muhammad.frontend <128728730+MagaFrontEnd@users.noreply.github.com>
2024-12-05 19:54:41 +0300

    two commit

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ Object
 */
 
 // вывести в консоль
-console.log("hello world!"); // String
+// console.log("hello world!"); // String
 
-let admin = "Muhammad";
-let age = 19;
+// let admin = "Muhammad";
+// let age = 19;
 
-console.log(admin, age);
+// console.log(admin, age);
 
 // let a = 30; // 40
 // let b = 10; // 30
@@ -26,5 +26,38 @@ console.log(admin, age);
 
 // console.log("a:", a, "b:", b);
 
-// let 1dfg = 123;
-// let -reset = '234';
+//Какой тип данных нужно использовать, чтобы задать дату формата 28.02.1999?
+// let date = "28.02.1999";
+// Нужен тип данных string/строка
+// console.log(date);
+
+// Какой тип данных нужен для сохранения числа ПИ 3.14?
+// let pi = 3.14;
+// console.log(pi);
+// Нужен тип данных number/строка
+
+// Создать константу с любым значением и попытаться изменить это значение. Выяснить что в итоге получится.
+// const password = "helloWorld!";
+// password = "byeWorld!";
+// console.log(password);
+// Изменить значение нельзя, будет ошибка
+
+/*
+ Создать переменные name, address, phone в которых с помощью prompt() будет храниться значения имени, адреса и номер телефона. 
+ Вывести в консоль текст “Студент . Адрес: , Номер телефона: _” где вместо пропусков будут соответствующие переменные. 
+*/
+
+// let name = prompt("Введите ваше имя:");
+// let address = prompt("Введите ваш адрес:");
+// let phone = prompt("Введите ваш номер телефона:");
+
+// console.log(`My name: ${name}, My adress: ${address}, My namber phone: ${phone}`);
+// готово
+
+// Программа чтобы узнать сколько долларов можно купить на количество твоих рублей
+// let myRubles = 20000;
+// let dollarsRub = 103.38;
+
+// let dollars = myRubles / dollarsRub;
+// console.log(`На ${myRubles} рублей можно купить ${dollars}`);
+// готово
