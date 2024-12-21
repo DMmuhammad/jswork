c2ea6e14d825890a977902642520b9a0a6a4f199
Muhammad.frontend <128728730+MagaFrontEnd@users.noreply.github.com>
2024-12-21 22:40:33 +0300

    js007

--- js/main.js
+++ js/main.js
@@ -1,41 +1,61 @@
-/* 
-String
-Number
-BigInt
-Boolean 
-Undefined
-Null 
-Symbol
-Object
-*/
+// const name = "Muhammad"; String
+// const age = 19; Number
+// const isDeveloper = true; Boolean
+// const power = 9999999999n; BigInt
+// const sign = Symbol(); Symbol
+// const something = undefined; Undefined
+
+// const user = { Object
+//   login: maga,
+//   password: 12345,
+// };
+
+// const data = null; Null
 
 // <> = == === >= <=  &&
+// + - * / ** %
+// typeof
 
-// let a = ["lorem", 123, true, "ipsum"];
-// console.log(a);
-// console.log(a.length); // Вывод длины массива
-// console.log("Первый элемент:", a[0]); // Вывод первого элемента
-// console.log("Последний элемент:", a[a.length - 1]); // Вывод последнего элемента
-// console.log(a.at(-1)); // Вывод последнего элемента второй вариант
+// =================================================== //
 
-// a.push("Ali");
-// a.push("Parkour");
-// a.push(95);
+// const numbers = [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
+// numbers.forEach((numbers) => {
+//   if (numbers % 2 !== 0) {
+//     console.log(numbers);
+//   }
+// });
 
-// let myArr = [];
+// const elements = ["lorem ipsum", "javascript", "php", "css", "react", "git", "html", "mysql"];
+// const filterElements = elements.filter(elements => elements.length <= 4);
+// console.log(filterElements);
 
-// myArr.push("Alishan");
-// myArr.push("Maga");
-// myArr.push(123);
-// myArr.push(true);
-// myArr.push(false);
+// const arr = [1, -3, 5, 6, -7, 8, 9, -11];
+// const minusArr = arr.filter((arr) => arr <= 0);
+// console.log(minusArr);
 
-// myArr.shift();
-// myArr.pop();
-// myArr.pop();
+// const arr = [5, 6, 7, 8, 9];
+// const bigArr = arr.map((arr) => arr * arr);
+// console.log(bigArr);
 
-// console.log(myArr);
+// const array = [
+//   {a: 10, b: 5},
+//   {a: 20, b: 22},
+//   {a: 131, b: 55},
+// ];
+// const result = array.map((array) => array.a + array.b);
+// console.log(result);
 
-// let number = [1, 3, 5, 7, 9];
-// let number2 = number.map((num) => num + 1);
-// console.log(number2);
+// const array = [-13, 0, 12, 1662, -0.32, -102, -2];
+// const mixArray = array.filter((num) => num <= 0).reduce((sum, num) => sum + num, 0);
+// console.log(mixArray);
+
+// const array = [
+//   {x: 10, y: "lorem"},
+//   {x: 21, y: "lorem"},
+//   {x: -17, y: "lorem"},
+//   {x: 156, y: "lorem"},
+// ];
+
+// const sum = array.reduce((sum, obj) => sum + obj.x, 0);
+
+// console.log(sum);
